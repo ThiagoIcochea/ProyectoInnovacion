952b3afa846e0f6f0e65f0a2725683ffb9454f82
ThiagoIcochea <U22330428@utp.edu.pe>
2026-05-12 01:09:42 -0500

    Backend listo 9

--- Backend/b2b/src/main/java/com/nethink/b2b/B2bApplication.java
+++ Backend/b2b/src/main/java/com/nethink/b2b/B2bApplication.java
@@ -13,14 +13,14 @@ public class B2bApplication {
 
 	public static void main(String[] args) {
             
-             Dotenv dotenv = Dotenv.configure()
+            /* Dotenv dotenv = Dotenv.configure()
                 .filename(".env")
                 .load();
 
         
         System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));*/
 		SpringApplication.run(B2bApplication.class, args);
 	}
 
