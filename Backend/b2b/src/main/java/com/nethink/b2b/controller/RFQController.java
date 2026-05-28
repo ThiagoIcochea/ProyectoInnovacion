4255001db356373fc9e43415ab6ceb08169c808e
ThiagoIcochea <U22330428@utp.edu.pe>
2026-05-28 11:25:28 -0500

    Ajustando backend para produccion de frontend

--- Backend/b2b/src/main/java/com/nethink/b2b/controller/RFQController.java
+++ Backend/b2b/src/main/java/com/nethink/b2b/controller/RFQController.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/rfq")
-@CrossOrigin(origins = "http://localhost:4200")
 public class RFQController {
 
     private final RFQService rfqService;
