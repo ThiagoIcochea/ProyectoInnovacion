b75fd8e0d6e95e8e4b5497a79e97ab13cf117c90
ThiagoIcochea <U22330428@utp.edu.pe>
2026-06-21 21:01:18 -0500

    Arreglando entregas

--- Frontend/src/app/features/client/request-tracking/request-tracking.ts
+++ Frontend/src/app/features/client/request-tracking/request-tracking.ts
@@ -181,7 +181,7 @@ export class RequestTrackingComponent implements OnInit {
     return this.normalizarEstado(this.tracking?.estado) === 'PAGO_PENDIENTE';
   }
    esEnCamino(): boolean {
-    
+    console.log("ESTADO ACTUAL:", this.tracking?.estado);
     return  this.tracking?.estado === 'EN_CAMINO';
   }
 }
