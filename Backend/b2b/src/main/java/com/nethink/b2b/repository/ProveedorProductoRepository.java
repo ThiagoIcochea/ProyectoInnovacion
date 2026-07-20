52d3224779972d6e9adf97fb1704c8542f5345c1
ThiagoIcochea <U22330428@utp.edu.pe>
2026-07-19 19:01:17 -0500

    Mejorando suscripciones

--- Backend/b2b/src/main/java/com/nethink/b2b/repository/ProveedorProductoRepository.java
+++ Backend/b2b/src/main/java/com/nethink/b2b/repository/ProveedorProductoRepository.java
@@ -31,17 +31,9 @@ public interface ProveedorProductoRepository
     @Query(value = """
         SELECT pp.id_proveedor
         FROM proveedor_producto pp
-        JOIN productos p
-            ON pp.id_producto = p.id_producto
-        JOIN proveedores pr
-            ON pp.id_proveedor = pr.id_proveedor
-        JOIN usuarios u
-            ON pr.id_usuario = u.id_usuario
         WHERE pp.id_producto IN :productos
         AND pp.stock > 0
         AND pp.estado = 'ACTIVO'
-        AND pr.estado = 'ACTIVO'
-        AND u.estado = 'ACTIVO'
         GROUP BY pp.id_proveedor
         HAVING COUNT(DISTINCT pp.id_producto) = :total
     """, nativeQuery = true)
@@ -53,15 +45,9 @@ public interface ProveedorProductoRepository
     @Query(value = """
         SELECT DISTINCT pp.id_proveedor
         FROM proveedor_producto pp
-        JOIN proveedores pr
-            ON pp.id_proveedor = pr.id_proveedor
-        JOIN usuarios u
-            ON pr.id_usuario = u.id_usuario
         WHERE pp.id_producto IN :productos
         AND pp.stock > 0
         AND pp.estado = 'ACTIVO'
-        AND pr.estado = 'ACTIVO'
-        AND u.estado = 'ACTIVO'
     """, nativeQuery = true)
     List<Integer> findProveedoresConAlgunProducto(
             @Param("productos") List<Integer> productos
